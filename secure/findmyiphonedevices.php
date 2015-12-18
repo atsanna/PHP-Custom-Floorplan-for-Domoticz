9c0e906f5072bec90f71ac9c3dd185a030d6e3a8
Egregius <guy@egregius.be>
2015-12-18 10:22:13 +0100

    Update

--- secure/findmyiphonedevices.php
+++ secure/findmyiphonedevices.php
@@ -1,7 +1,7 @@
 <?PHP
 	error_reporting(0);
 	header("Content-type: text/html; charset=utf-8");
-	require_once "ios.php";
+	require_once "functions.php";
 	require_once "findmyiphone.php";
 	try {
 		$fmi = new FindMyiPhone($appleid, $applepass);
